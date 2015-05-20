d0e771c246863e7bf42da6a1c400603ec5974ba6
tlelaxu <kenneth0@live.be>
2015-05-20 22:14:17 +0200

    added all the things

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,21 @@
+import XMonad
+
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
+import XMonad.Hooks.DynamicLog
+
+main = xmonad =<< xmobar myConfig
+
+myConfig = defaultConfig 
+    { modMask = mod4Mask
+    , terminal = "xterm"
+    } `additionalKeysP`
+                   [ ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
+                   , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10")
+                   , ("<XF86AudioRaiseVolume>", spawn "pulseaudio-ctl up")
+                   , ("<XF86AudioLowerVolume>", spawn "pulseaudio-ctl down")
+                   , ("<XF86AudioMute>", spawn "pulseaudio-ctl mute")
+                   , ("<Print>", spawn "scrot -e 'mv $f ~/Pictures/screenshots'")
+                   , ("M-l", spawn "xsecurelock")
+                   , ("M-c", spawn "chromium")
+                   ]
